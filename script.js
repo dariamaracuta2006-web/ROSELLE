aec7a1ce69c3bd783745a1e6cab2e38626e7db8f
dariamaracuta2006-web <dariamaracuta2006@gmail.com>
2026-05-05 00:36:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-alert("JavaScript funcționează!");
+console.log("JS merge");
+
+// efect pe produse
+const cards = document.querySelectorAll(".product-card");
+
+cards.forEach(function(card) {
+  card.addEventListener("mouseover", function() {
+    card.style.transform = "scale(1.05)";
+    card.style.transition = "0.3s";
+  });
+
+  card.addEventListener("mouseout", function() {
+    card.style.transform = "scale(1)";
+  });
+});
+
+// efect pe butoane
+const buttons = document.querySelectorAll(".btn, .product-btn");
+
+buttons.forEach(function(btn) {
+  btn.addEventListener("mouseover", function() {
+    btn.style.opacity = "0.8";
+  });
+
+  btn.addEventListener("mouseout", function() {
+    btn.style.opacity = "1";
+  });
+});
